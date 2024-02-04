35413e4e1aab37e74ae1e088426f1b7375a5191d
glopesdev <glopesdev@users.noreply.github.com>
2024-02-04 15:38:58 +0000

    Avoid method redeclaration

--- src/Bonsai.Gui/ControlBuilderBase.cs
+++ src/Bonsai.Gui/ControlBuilderBase.cs
@@ -88,7 +88,7 @@ namespace Bonsai.Gui
     /// </summary>
     /// <typeparam name="TCommand">The type of command notifications accepted by the UI control.</typeparam>
     /// <typeparam name="TEvent">The type of event notifications emitted by the UI control.</typeparam>
-    public abstract class ControlBuilderBase<TCommand, TEvent> : ControlBuilderBase, INamedElement
+    public abstract class ControlBuilderBase<TCommand, TEvent> : ControlBuilderBase<TEvent>, INamedElement
     {
         static readonly Range<int> argumentRange = Range.Create(lowerBound: 0, upperBound: 1);
 
@@ -107,14 +107,6 @@ namespace Bonsai.Gui
             var commands = source == null ? Array.Empty<Expression>() : new[] { source };
             return Expression.Call(Expression.Constant(this), nameof(Generate), null, commands);
         }
-
-        /// <summary>
-        /// Generates an observable sequence of event notifications from the UI control.
-        /// </summary>
-        /// <returns>
-        /// An observable sequence of events of type <typeparamref name="TEvent"/>.
-        /// </returns>
-        protected abstract IObservable<TEvent> Generate();
 
         /// <summary>
         /// Generates an observable sequence of event notifications from the UI control.
