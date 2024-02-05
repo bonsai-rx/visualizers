31c0d6fbb7a9517953a4774a3ecfb58420d49b3b
glopesdev <glopesdev@users.noreply.github.com>
2024-02-05 00:01:34 +0000

    Add common button controls

---
+++ src/Bonsai.Gui/RadioButtonBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Subjects;
+
+namespace Bonsai.Gui
+{
+    /// <summary>
+    /// Represents an operator that interfaces with a radio button control and generates
+    /// a sequence of notifications whenever the checked status changes.
+    /// </summary>
+    [TypeVisualizer(typeof(RadioButtonVisualizer))]
+    [Description("Interfaces with a radio button control and generates a sequence of notifications whenever the checked status changes.")]
+    public class RadioButtonBuilder : ButtonBuilderBase<bool>
+    {
+        internal readonly Subject<bool> _CheckedChanged = new();
+
+        /// <summary>
+        /// Gets or sets a value specifying the initial state of the radio button.
+        /// </summary>
+        [Category(nameof(CategoryAttribute.Appearance))]
+        [Description("Specifies the initial state of the radio button.")]
+        public bool Checked { get; set; }
+
+        /// <inheritdoc/>
+        protected override IObservable<bool> Generate()
+        {
+            return _CheckedChanged;
+        }
+    }
+}
---
+++ src/Bonsai.Gui/RadioButtonVisualizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bonsai.Gui
+{
+    /// <summary>
+    /// Provides a type visualizer representing a radio button control.
+    /// </summary>
+    public class RadioButtonVisualizer : ControlVisualizerBase<RadioButton, RadioButtonBuilder>
+    {
+        /// <inheritdoc/>
+        protected override RadioButton CreateControl(IServiceProvider provider, RadioButtonBuilder builder)
+        {
+            var radioButton = new RadioButton();
+            radioButton.Dock = DockStyle.Fill;
+            radioButton.Size = new Size(300, 75);
+            radioButton.Checked = builder.Checked;
+            radioButton.SubscribeTo(builder._Text, value => radioButton.Text = value);
+            radioButton.CheckedChanged += (sender, e) =>
+            {
+                builder._CheckedChanged.OnNext(radioButton.Checked);
+            };
+            return radioButton;
+        }
+    }
+}
