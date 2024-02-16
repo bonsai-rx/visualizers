cc32cc2f8388fed4f1f03fd3039ad6c1d2b3eba3
glopesdev <glopesdev@users.noreply.github.com>
2024-02-16 00:43:14 +0000

    Add property grid control

---
+++ src/Bonsai.Gui/PropertyGridBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Reactive.Subjects;
+
+namespace Bonsai.Gui
+{
+    /// <summary>
+    /// Represents an operator that interfaces with a property grid control for
+    /// browsing the properties of the workflow in which it is inserted.
+    /// </summary>
+    [TypeVisualizer(typeof(PropertyGridVisualizer))]
+    [Description("Interfaces with a property grid control for browsing the properties of the workflow in which it is inserted.")]
+    public class PropertyGridBuilder : ControlBuilderBase
+    {
+        internal readonly BehaviorSubject<bool> _HelpVisible = new(true);
+        internal readonly BehaviorSubject<bool> _ToolbarVisible = new(true);
+
+        /// <summary>
+        /// Gets or sets a value specifying whether the help text box is visible.
+        /// </summary>
+        [Description("Specifies whether the help text box is visible.")]
+        public bool HelpVisible
+        {
+            get => _HelpVisible.Value;
+            set => _HelpVisible.OnNext(value);
+        }
+
+        /// <summary>
+        /// Gets or sets a value specifying whether the toolbar is visible.
+        /// </summary>
+        [Description("Specifies whether the toolbar is visible.")]
+        public bool ToolbarVisible
+        {
+            get => _ToolbarVisible.Value;
+            set => _ToolbarVisible.OnNext(value);
+        }
+    }
+}
---
+++ src/Bonsai.Gui/PropertyGridVisualizer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Bonsai.Expressions;
+using PropertyGrid = Bonsai.Design.PropertyGrid;
+
+namespace Bonsai.Gui
+{
+    /// <summary>
+    /// Provides a type visualizer representing a property grid control.
+    /// </summary>
+    public class PropertyGridVisualizer : ControlVisualizerBase<PropertyGrid, PropertyGridBuilder>
+    {
+        /// <inheritdoc/>
+        protected override PropertyGrid CreateControl(IServiceProvider provider, PropertyGridBuilder builder)
+        {
+            var propertyGrid = new PropertyGrid();
+            propertyGrid.Dock = DockStyle.Fill;
+            propertyGrid.Size = new Size(350, 450);
+            propertyGrid.Site = new ServiceProviderContext(provider);
+            propertyGrid.SubscribeTo(builder._HelpVisible, value => propertyGrid.HelpVisible = value);
+            propertyGrid.SubscribeTo(builder._ToolbarVisible, value => propertyGrid.ToolbarVisible = value);
+
+            var workflowBuilder = (WorkflowBuilder)provider.GetService(typeof(WorkflowBuilder));
+            propertyGrid.SelectedObject = GetExpressionContext(workflowBuilder.Workflow, builder);
+            return propertyGrid;
+        }
+
+        static ExpressionBuilderGraph GetExpressionContext(ExpressionBuilderGraph source, ExpressionBuilder target)
+        {
+            foreach (var element in source.Elements())
+            {
+                if (element == target) return source;
+                if (element is IWorkflowExpressionBuilder workflowBuilder)
+                {
+                    var nestedContext = GetExpressionContext(workflowBuilder.Workflow, target);
+                    if (nestedContext != null)
+                    {
+                        return nestedContext;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        class ServiceProviderContext : ISite
+        {
+            readonly IServiceProvider parentProvider;
+
+            public ServiceProviderContext(IServiceProvider provider)
+            {
+                parentProvider = provider;
+            }
+
+            public IComponent Component => null;
+
+            public IContainer Container => null;
+
+            public bool DesignMode => false;
+
+            public string Name { get; set; }
+
+            public object GetService(Type serviceType)
+            {
+                return parentProvider?.GetService(serviceType);
+            }
+        }
+    }
+}
