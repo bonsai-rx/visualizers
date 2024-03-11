55ea48f0966b127b73041faa82beeccdda6b2d37
glopesdev <glopesdev@users.noreply.github.com>
2024-03-11 12:44:06 +0000

    Add slider control

---
+++ src/Bonsai.Gui/SliderVisualizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Bonsai.Design;
+
+namespace Bonsai.Gui
+{
+    /// <summary>
+    /// Provides a type visualizer representing a slider control.
+    /// </summary>
+    public class SliderVisualizer : ControlVisualizerBase<Slider, SliderBuilder>
+    {
+        /// <inheritdoc/>
+        protected override Slider CreateControl(IServiceProvider provider, SliderBuilder builder)
+        {
+            var slider = new Slider();
+            slider.Dock = DockStyle.Fill;
+            slider.Size = new Size(300, slider.Height);
+            slider.SubscribeTo(builder._Minimum, value => slider.Minimum = value);
+            slider.SubscribeTo(builder._Maximum, value => slider.Maximum = value);
+            slider.SubscribeTo(builder._DecimalPlaces, value => slider.DecimalPlaces = value);
+            slider.SubscribeTo(builder._Value, value => slider.Value = builder.Value);
+            slider.ValueChanged += (sender, e) =>
+            {
+                builder._Value.OnNext(slider.Value);
+            };
+            return slider;
+        }
+    }
+}
