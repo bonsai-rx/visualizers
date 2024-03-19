8b1fa446f24b4c73909e6a1289d9458b92b258f4
glopesdev <glopesdev@users.noreply.github.com>
2024-03-19 06:41:16 +0000

    Add graph panel visualizer

---
+++ src/Bonsai.Gui.Visualizers/GraphPanelBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using System.Reactive;
+using Bonsai.Expressions;
+using ZedGraph;
+
+namespace Bonsai.Gui.Visualizers
+{
+    /// <summary>
+    /// Represents an operator that specifies a mashup graph panel that can be used
+    /// to combine multiple plots sharing the same axes.
+    /// </summary>
+    [TypeVisualizer(typeof(GraphPanelVisualizer))]
+    public class GraphPanelBuilder : ZeroArgumentExpressionBuilder, INamedElement
+    {
+        /// <summary>
+        /// Gets or sets the name of the visualizer window.
+        /// </summary>
+        [Category(nameof(CategoryAttribute.Design))]
+        [Description("The name of the visualizer window.")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value specifying the axis on which the bars in the graph will be displayed.
+        /// </summary>
+        [TypeConverter(typeof(BaseAxisConverter))]
+        [Category(nameof(CategoryAttribute.Appearance))]
+        [Description("Specifies the axis on which the bars in the graph will be displayed.")]
+        public BarBase BaseAxis { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value specifying how the different bars in the graph will be visually arranged.
+        /// </summary>
+        [Category(nameof(CategoryAttribute.Appearance))]
+        [Description("Specifies how the different bars in the graph will be visually arranged.")]
+        public BarType BarType { get; set; }
+
+        /// <summary>
+        /// Builds the expression tree for configuring and calling the
+        /// graph panel visualizer.
+        /// </summary>
+        /// <inheritdoc/>
+        public override Expression Build(IEnumerable<Expression> arguments)
+        {
+            return Expression.Call(typeof(Observable), nameof(Observable.Never), new[] { typeof(Unit) });
+        }
+    }
+}
---
+++ src/Bonsai.Gui.Visualizers/GraphPanelVisualizer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Bonsai.Design;
+using ZedGraph;
+
+namespace Bonsai.Gui.Visualizers
+{
+    /// <summary>
+    /// Provides a type visualizer that can be used to overlay multiple plots sharing
+    /// the same axes in a single graph panel.
+    /// </summary>
+    public class GraphPanelVisualizer : MashupControlVisualizerBase<GraphControl, GraphPanelBuilder>
+    {
+        Type indexType;
+        BarSettings barSettings;
+        GraphPanelBuilder graphBuilder;
+
+        static void ThrowIfNotEquals<T>(T left, T right, string message)
+        {
+            if (!EqualityComparer<T>.Default.Equals(left, right))
+            {
+                throw new InvalidOperationException(message);
+            }
+        }
+
+        internal Axis BarBaseAxis()
+        {
+            return graphBuilder.BaseAxis switch
+            {
+                BarBase.Y => Control.GraphPane.YAxis,
+                BarBase.Y2 => Control.GraphPane.Y2Axis,
+                BarBase.X2 => Control.GraphPane.X2Axis,
+                _ => Control.GraphPane.XAxis
+            };
+        }
+
+        internal void EnsureIndex(Type type)
+        {
+            if (indexType == null)
+            {
+                indexType = type;
+                var baseAxis = BarBaseAxis();
+                if (type == typeof(string))
+                {
+                    GraphHelper.FormatOrdinalAxis(baseAxis, indexType);
+                }
+                if (type == typeof(XDate))
+                {
+                    GraphHelper.FormatLinearDateAxis(baseAxis);
+                }
+            }
+            else ThrowIfNotEquals(indexType, type, "Only overlays with identical axis are allowed.");
+        }
+
+        internal void EnsureBarSettings(BarSettings settings)
+        {
+            if (barSettings == null)
+            {
+                barSettings = Control.GraphPane.BarSettings;
+                barSettings.Base = settings.Base;
+                barSettings.ClusterScaleWidth = settings.ClusterScaleWidth;
+                barSettings.ClusterScaleWidthAuto = settings.ClusterScaleWidthAuto;
+                barSettings.MinBarGap = settings.MinBarGap;
+                barSettings.MinClusterGap = settings.MinClusterGap;
+                barSettings.Type = settings.Type;
+            }
+            else
+            {
+                const string ErrorMessage = "All bar graph overlays must have identical settings.";
+                ThrowIfNotEquals(barSettings.Base, settings.Base, ErrorMessage);
+                ThrowIfNotEquals(barSettings.ClusterScaleWidth, settings.ClusterScaleWidth, ErrorMessage);
+                ThrowIfNotEquals(barSettings.ClusterScaleWidthAuto, settings.ClusterScaleWidthAuto, ErrorMessage);
+                ThrowIfNotEquals(barSettings.MinBarGap, settings.MinBarGap, ErrorMessage);
+                ThrowIfNotEquals(barSettings.MinClusterGap, settings.MinClusterGap, ErrorMessage);
+                ThrowIfNotEquals(barSettings.Type, settings.Type, ErrorMessage);
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override GraphControl CreateControl(IServiceProvider provider, GraphPanelBuilder builder)
+        {
+            var graph = new GraphControl();
+            graph.Dock = DockStyle.Fill;
+            graphBuilder = builder;
+            return graph;
+        }
+
+        /// <inheritdoc/>
+        protected override void LoadMashupSource(int index, MashupSource mashupSource, IServiceProvider provider)
+        {
+            Control.ResetColorCycle();
+            base.LoadMashupSource(index, mashupSource, provider);
+        }
+
+        /// <inheritdoc/>
+        public override void Unload()
+        {
+            base.Unload();
+            indexType = null;
+            barSettings = null;
+            graphBuilder = null;
+        }
+    }
+}
