154dde1072e7fb88f4daf2ded085d225b5d6b5cf
glopesdev <glopesdev@users.noreply.github.com>
2024-03-25 18:20:55 +0000

    Rename ZedGraph visualizers package

---
+++ src/Bonsai.Gui.ZedGraph/LineGraphVisualizer.cs
@@ -0,0 +1,197 @@
+﻿using Bonsai.Design;
+using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Bonsai.Gui.ZedGraph
+{
+    /// <summary>
+    /// Provides a type visualizer to display an object as a line graph.
+    /// </summary>
+    public class LineGraphVisualizer : BufferedVisualizer, ILineGraphVisualizer
+    {
+        LineGraphBuilder.VisualizerController controller;
+        LineGraphView view;
+        bool labelLines;
+        bool reset;
+
+        /// <summary>
+        /// Gets or sets the maximum number of points displayed at any one moment in the graph.
+        /// </summary>
+        public int Capacity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the lower limit of the X-axis range when using a fixed scale.
+        /// </summary>
+        public double XMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the upper limit of the X-axis range when using a fixed scale.
+        /// </summary>
+        public double XMax { get; set; } = 1;
+
+        /// <summary>
+        /// Gets or sets the lower limit of the Y-axis range when using a fixed scale.
+        /// </summary>
+        public double YMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the upper limit of the Y-axis range when using a fixed scale.
+        /// </summary>
+        public double YMax { get; set; } = 1;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the X-axis range should be recalculated
+        /// automatically as the graph updates.
+        /// </summary>
+        public bool AutoScaleX { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Y-axis range should be recalculated
+        /// automatically as the graph updates.
+        /// </summary>
+        public bool AutoScaleY { get; set; } = true;
+
+        void ILineGraphVisualizer.AddValues(double index, params PointPair[] values)
+        {
+            if (view.Graph.NumSeries != values.Length || reset)
+            {
+                view.Graph.EnsureCapacity(
+                    values.Length,
+                    labelLines ? controller.ValueLabels : null,
+                    reset);
+                reset = false;
+            }
+            view.Graph.AddValues(index, values);
+        }
+
+        /// <inheritdoc/>
+        public override void Load(IServiceProvider provider)
+        {
+            var context = (ITypeVisualizerContext)provider.GetService(typeof(ITypeVisualizerContext));
+            var lineChartBuilder = (LineGraphBuilder)ExpressionBuilder.GetVisualizerElement(context.Source).Builder;
+            controller = lineChartBuilder.Controller;
+
+            view = new LineGraphView();
+            view.Dock = DockStyle.Fill;
+            view.Graph.LineWidth = controller.LineWidth;
+            view.Graph.SymbolType = controller.SymbolType;
+            labelLines = controller.ValueLabels != null;
+            if (labelLines && controller.LabelAxes && controller.ValueLabels.Length == 2)
+            {
+                labelLines = false;
+                GraphHelper.SetAxisLabel(view.Graph.GraphPane.XAxis, controller.ValueLabels[0]);
+                GraphHelper.SetAxisLabel(view.Graph.GraphPane.YAxis, controller.ValueLabels[1]);
+            }
+
+            if (labelLines)
+            {
+                view.Graph.EnsureCapacity(
+                    controller.ValueLabels.Length,
+                    labelLines ? controller.ValueLabels : null);
+            }
+
+            if (controller.XMin.HasValue || controller.XMax.HasValue)
+            {
+                view.AutoScaleX = false;
+                view.AutoScaleXVisible = false;
+                view.XMin = controller.XMin.GetValueOrDefault();
+                view.XMax = controller.XMax.GetValueOrDefault();
+            }
+            else
+            {
+                view.AutoScaleX = AutoScaleX;
+                if (!AutoScaleX)
+                {
+                    view.XMin = XMin;
+                    view.XMax = XMax;
+                }
+            }
+
+            if (controller.YMin.HasValue || controller.YMax.HasValue)
+            {
+                view.AutoScaleY = false;
+                view.AutoScaleYVisible = false;
+                view.YMin = controller.YMin.GetValueOrDefault();
+                view.YMax = controller.YMax.GetValueOrDefault();
+            }
+            else
+            {
+                view.AutoScaleY = AutoScaleY;
+                if (!AutoScaleY)
+                {
+                    view.YMin = YMin;
+                    view.YMax = YMax;
+                }
+            }
+
+            if (controller.Capacity.HasValue)
+            {
+                view.Capacity = controller.Capacity.Value;
+                view.CanEditCapacity = false;
+            }
+            else
+            {
+                view.Capacity = Capacity;
+                view.CanEditCapacity = true;
+            }
+
+            view.HandleDestroyed += delegate
+            {
+                XMin = view.XMin;
+                XMax = view.XMax;
+                YMin = view.YMin;
+                YMax = view.YMax;
+                AutoScaleX = view.AutoScaleX;
+                AutoScaleY = view.AutoScaleY;
+                Capacity = view.Capacity;
+            };
+
+            var visualizerService = (IDialogTypeVisualizerService)provider.GetService(typeof(IDialogTypeVisualizerService));
+            if (visualizerService != null)
+            {
+                visualizerService.AddControl(view);
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void ShowBuffer(IList<Timestamped<object>> values)
+        {
+            base.ShowBuffer(values);
+            if (values.Count > 0)
+            {
+                view.Graph.Invalidate();
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void Show(object value)
+        {
+            Show(DateTime.Now, value);
+        }
+
+        /// <inheritdoc/>
+        protected override void Show(DateTime time, object value)
+        {
+            controller.AddValues(time, value, this);
+        }
+
+        /// <inheritdoc/>
+        public override void SequenceCompleted()
+        {
+            reset = true;
+        }
+
+        /// <inheritdoc/>
+        public override void Unload()
+        {
+            view.Dispose();
+            view = null;
+            controller = null;
+            reset = false;
+        }
+    }
+}
---
+++ src/Bonsai.Gui.ZedGraph/ThrowHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bonsai.Gui.ZedGraph
+{
+    static class ThrowHelper
+    {
+        public static void ThrowArgumentOutOfRange(string paramName)
+        {
+            throw new ArgumentOutOfRangeException(paramName);
+        }
+
+        public static void ThrowArgumentNull(string paramName)
+        {
+            throw new ArgumentNullException(paramName);
+        }
+
+        public static void ThrowIfNotEquals<T>(T left, T right, string message)
+        {
+            if (!EqualityComparer<T>.Default.Equals(left, right))
+            {
+                throw new InvalidOperationException(message);
+            }
+        }
+    }
+}
